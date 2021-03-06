bf5e90f64eebb0a93e568bb8e7f1d1aec8a01aef
Tzikas <niko.tzikas@gmail.com>
2021-03-06 09:46:35 -0500

    added kittys

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
 import { Component } from 'react'
+import axios from 'axios'
 
 class Home extends Component {
+
+    state = {
+        catUrl: ''
+    }
+
+    componentDidMount() {
+        axios
+            .get(`https://api.thecatapi.com/v1/images/search`)
+            .then(res => {
+                console.log(res.data[0].url)
+                this.setState({
+                    catUrl: res.data[0].url
+                })
+            })
+    }
 
     render() {
         return (
             <>
                 <h1>Home</h1>
+                <img width="500px" src={this.state.catUrl} />
             </>
         )
     }
